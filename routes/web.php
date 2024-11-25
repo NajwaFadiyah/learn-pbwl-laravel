dd139bb2e379c6ba177b2cd51d368a703082489e
NajwaFadiyah <fadiyahn30@gmail.com>
2024-11-25 08:33:43 +0700

    Belajar While

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,17 @@ Route::get('profil', function () {
     $nama = "Junior";
     return view('profile', compact('nama'));
 });
+
+// To understand how to use array, and benefit of array
+Route::get('array', function () {
+    // Contoh penggunaan while
+
+    // Infinite loop (perulangan yang tidak akan berhenti karena bernilai true)
+    // Tentukan nilai falsenya agar berhenti
+    $nilai_awal = 1;
+    while ($nilai_awal <= 5) {
+        echo 'SELAMAT DATANG ' . $nilai_awal . 'x <br>';
+        // Nilai awal bertambah 1
+        $nilai_awal++;
+    }
+});
